f271e7eca62dbc25509d934c6d0e7a1af24bd8d3
Isaac Van Doren <69181572+isaacvando@users.noreply.github.com>
2023-03-02 16:09:45 -0600

    call spotify

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,71 @@
 module Main exposing (..)
 
-import Browser exposing (Document, document)
-import Html exposing (h1, text, div)
+import Browser
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import Url
+import Browser.Navigation as Nav
+import Url.Builder as Bld
 
-main = document
+main = Browser.application
   {
     init = init,
     view = view,
     update = update, 
-    subscriptions = subscriptions
+    subscriptions = subs,
+    onUrlRequest = onUrlRequest,
+    onUrlChange = onUrlChange
   }
 
 
 type alias Model = {}
 
-type Msg = Msg
+type Msg = RequestedAuth
 
 
-init : () -> (Model, Cmd Msg)
-init _ = ({}, Cmd.none)
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ _ _ = ({}, Cmd.none)
 
-view : Model -> Document Msg
-view _ = 
+
+view : Model -> Browser.Document Msg
+view model = 
   {
   title = "True Shuffle"
-  , body = [h1 [] [text "True Shuffle for Spotify"]]
+  , body = viewBody model
   }
 
-update : Msg -> Model -> (Model, Cmd Msg)
-update _ model = (model, Cmd.none)
 
-subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+viewBody : Model -> List (Html Msg)
+viewBody _ = [
+  h1 [] [text "True Shuffle for Spotify"]
+  , button [onClick RequestedAuth] [text "Authenticate TrueShuffle"]]
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model = case msg of
+  RequestedAuth -> (model, Nav.load authUrl)
+
+
+authUrl : String
+authUrl = Bld.crossOrigin "https://accounts.spotify.com/authorize" [] 
+  [
+  Bld.string "client_id" "0cdc0205c0184f809fa13c8f71d7848c"
+  , Bld.string "redirect_uri" "http://127.0.0.1:5500/index.html"
+  , Bld.string "response_type" "code"
+  ]
+
+
+
+
+subs : Model -> Sub Msg
+subs _ = Sub.none
+
+
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest _ = RequestedAuth
+
+
+onUrlChange : Url.Url -> Msg
+onUrlChange _ = RequestedAuth
+
+
